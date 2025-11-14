1effc946ade7105da30388a4f8c57a3b41715f99
José Antonio Parra <josparbar@gmail.com>
2025-11-14 11:06:02 +0100

    Upgrade to Ruby 3.1.4

---
+++ config/initializers/wicked_pdf.rb
@@ -0,0 +1,17 @@
+# WickedPdf Global Configuration
+#
+# Use this to set up shared configuration options for your entire application.
+# Any of the configuration options shown here can also be applied to single
+# models by passing arguments to the `render :pdf` call.
+#
+# To learn more, check out the README:
+#
+# https://github.com/mileszs/wicked_pdf/blob/master/README.md
+
+WickedPdf.configure do |c|
+  # Path to the wkhtmltopdf executable: This usually isn't needed if using
+  # one of the wkhtmltopdf-binary family of gems.
+  # exe_path: '/usr/local/bin/wkhtmltopdf',
+  #   or
+  # exe_path: Gem.bin_path('wkhtmltopdf-binary-edge', 'wkhtmltopdf')
+end
